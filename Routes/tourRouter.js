ffb9d19f22a6f06a55fec764a630392c193ee2b6
Adel Farouk <farouk.adel931@gmail.com>
2023-04-17 00:08:38 +0200

    Another error

--- Routes/tourRouter.js
+++ Routes/tourRouter.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-const tourController=require(`${__dirname}/../Controllers/tourController`);
+const tourController=require(`./../Controllers/tourController`);
 const authController=require('./../Controllers/authController');
 
 //  router.param('id',tourController.checkId);
