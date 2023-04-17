cd252dd4a2e8a2e320d1f1e9741b3418a2bb3c9e
Adel Farouk <farouk.adel931@gmail.com>
2023-04-17 15:33:41 +0200

    Data Sanitize

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
-const rateLimit=require('express-rate-limit');
+const rateLimit=require('express-rate-limit'); // security
+const helmet=require('helmet'); // security
+const mongoSanitize=require('express-mongo-sanitize'); // security
+const xss=require('xss-clean'); // security
+const hpp=require('hpp'); // security
 const AppError=require('./utils/appError');
 const tourRouter=require('./Routes/tourRouter');
 const userRouter=require('./Routes/userRouter');
@@ -8,9 +12,18 @@ const globalErrorHandler=require('./controllers/errorController');
 const app = express();
 
 // Global MiddleWares
+
+//set security http headers 
+app.use(helmet()) // set el htttp headers property 
+  
+
+//development logging
 if(process.env.NODE_ENV==='development'){
 app.use(morgan('dev'));
 }
+
+
+//Limit requests from same API
 // hna bn3ml limitng l3dd el mrat elly log in 34an  el brute force attacks
 const limiter =rateLimit({
   max:100,
@@ -18,16 +31,44 @@ const limiter =rateLimit({
   message:'too many requests please try again later'
 })
 
+
 app.use('/api',limiter) // (/api)=> all routes start with /api
 
+//Body parser,reading data from body into req.body
 app.use(express.json()); //middle ware for req,res json files 3and req.body
 
+//Data sanitization against no SQL injection
+app.use(mongoSanitize());
+
+//Data sanitization against cross site scripting attacks (XSS)
+app.use(xss());
+
+//prevent parameter pollution
+app.use(hpp({
+  whitelist:[
+    'duration',
+    'maxGroupSize',
+    'difficulty',
+    'ratingsAverage',
+    'ratingsQuantity',
+    'price',
+    
+
+] // loo fe al apifeatures 7t 2 value fe el query yasr4 3la kolo
+}));
+
+//serving static files
+app.use(express.static(`${__dirname}/public`));
+
+//app.use(express.json({limit:'10kb'})); => limit of data in body not more than 10 KB
+
+//request time of API
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
   next();
 });
-//Data
+
 
 
 
