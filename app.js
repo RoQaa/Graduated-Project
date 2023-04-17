8cae61d188ab7a7a12add107cff553289cd4e8a5
Adel Farouk <farouk.adel931@gmail.com>
2023-04-17 03:45:02 +0200

    Handling bruteforce attacks in app js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
-const AppError=require(`${__dirname}/utils/appError`);
-const tourRouter=require(`${__dirname}/Routes/tourRouter`);
-const userRouter=require(`${__dirname}/Routes/userRouter`);
-const globalErrorHandler=require(`${__dirname}/controllers/errorController`);
+const rateLimit=require('express-rate-limit');
+const AppError=require('./utils/appError');
+const tourRouter=require('./Routes/tourRouter');
+const userRouter=require('./Routes/userRouter');
+const globalErrorHandler=require('./controllers/errorController');
 const app = express();
 
-//MiddleWares
-
+// Global MiddleWares
+if(process.env.NODE_ENV==='development'){
 app.use(morgan('dev'));
+}
+// hna bn3ml limitng l3dd el mrat elly log in 34an  el brute force attacks
+const limiter =rateLimit({
+  max:100,
+  windowMs:60*60*1000,
+  message:'too many requests please try again later'
+})
+app.use('/api',limiter) // (/api)=> all routes start with /api
 app.use(express.json()); //middle ware for req,res json files 3and req.body
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
