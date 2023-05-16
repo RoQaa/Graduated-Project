624c446d36ecc38263ed16c918d5bfe1f4be91f7
Adel Farouk <farouk.adel931@gmail.com>
2023-05-16 13:37:32 +0300

    edit post router

--- Routes/postRouter.js
+++ Routes/postRouter.js
@@ -4,6 +4,6 @@ const authController=require(`${__dirname}/../controllers/authController`);
 const postController=require('../controllers/postController');
 
 router.post('/add',authController.protect,postController.addPost);
-router.get('/all',postController.getPosts);
+router.get('/postTimeLine',postController.getPosts);
 router.route('/delete/:id').delete(postController.deletePost)
 module.exports=router;
