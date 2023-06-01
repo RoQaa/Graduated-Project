57b24bb9cfbc5ab8c9d12d450247923c8296411b
Adel Farouk <farouk.adel931@gmail.com>
2023-06-01 15:49:47 +0300

    add new import in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
-
+import { dirname, extname, resolve as resolvePath } from 'node:path';
  process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
    console.log(err.name, err.message);
